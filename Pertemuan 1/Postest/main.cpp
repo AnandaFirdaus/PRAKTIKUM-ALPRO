8add0216e64a1d3265fe9be8e8da60d2fb64c054
AnandaFirdaus <firdausananda08@gmail.com>
2022-03-18 21:31:48 +0700

    membuat main-cpp

---
+++ Pertemuan 1/Postest/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char lagi;
+    float x,y;
+    int pil;
+
+    menu :
+    system ("cls");
+    cout << "=========================\n";
+    cout << "Kalkulator Sederhana C++ \n";
+    cout << "=========================\n";
+    cout << "Pilihan Operator\n";
+    cout << "1.Penjumlahan\n";
+    cout << "2.Pengurangan\n";
+    cout << "3.Pembagian\n";
+    cout << "4.Perkalian\n";
+    cout << "Masukkan Kode Operator : ";cin >> pil;
+}
