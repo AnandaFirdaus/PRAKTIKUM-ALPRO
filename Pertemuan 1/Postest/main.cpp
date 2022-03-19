b6d5a120e2a97a2a1213800156631af870f70cc0
dolasrirejeki <dola.srirejeki9@gmail.com>
2022-03-19 20:39:41 +0700

    membuat main-cpp baru

--- Pertemuan 1/Postest/main.cpp
+++ Pertemuan 1/Postest/main.cpp
@@ -17,4 +17,28 @@ int main(){
     cout << "3.Pembagian\n";
     cout << "4.Perkalian\n";
     cout << "Masukkan Kode Operator : ";cin >> pil;
-}
+    if (pil > 4 || pil < 1){
+		cout << "\nKode yang anda masukkan tidak ada \nSilahkan masukkan ulang kode .";
+		system("pause");
+        goto menu;
+    }
+    else {
+        cout << "Angka Pertama : ";cin >> x;
+        cout << "Angka Kedua : ";cin >> y;
+
+    if (pil == 1){
+	cout << "\nHasil penjumlahan dari "<<x<<" + "<<y<<" adalah : "<<x+y<<endl;
+	cout << "\nMasih lanjut [y/t]? ";
+		cin >> lagi;
+		if (lagi == 'y')
+		goto menu;
+		else return 0;
+        }
+    else if (pil == 2){
+            cout << "\nHasil pengurangan dari "<<x<<" - "<<y<<" adalah : "<<x-y<<endl;
+            cout << "\nMasih lanjut [y/t]? ";
+		cin >> lagi;
+		if (lagi == 'y')
+		goto menu;
+		else return 0;
+        }
