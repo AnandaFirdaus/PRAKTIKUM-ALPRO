b252471b9d79ca772ae5894b59e0dd379ebd0819
Ananda Firdaus <101865423+AnandaFirdaus@users.noreply.github.com>
2022-03-26 07:22:27 +0700

    Update Studi Kasus SPBU Baru

--- Studi Kasus SPBU.cpp
+++ Studi Kasus SPBU.cpp
@@ -35,3 +35,32 @@ void Bensin::pilihan(){
      hitung_uang();   
      }
  }
+void Bensin::hitung_uang(){
+     cout<<"masukkan jumlah uang : ";
+     cin>> uang;
+     if(ambil == '1')
+	 {
+	 	cout << "==============================" << endl;
+        cout << "Nama : " << nama << endl;
+		cout << "Jenis Bensin : Pertalite" << endl;
+		harga = pertalite;
+     }
+     else if(ambil == '2')
+	 {
+	 	cout << "==============================" << endl;
+	 	cout << "Nama : " << nama << endl;
+		cout << "Jenis Bensin : Pertamax" << endl;
+        harga = pertamax;
+     }
+     tampil = uang / harga;
+     cout << "Total Bayar : " << uang << endl;
+     cout<<"jumlah Bensin : "<<tampil <<" liter"<<endl;
+    cout << "==============================" << endl;
+     }
+
+int main()
+{
+    Bensin x;
+    x.pilihan();
+    return 0;
+}
