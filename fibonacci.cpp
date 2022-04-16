230673120cef317af3633218c264edd9fc6ecea6
Ananda Firdaus <101865423+AnandaFirdaus@users.noreply.github.com>
2022-04-16 18:42:46 +0700

    Fibonacci.cpp New

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -28,3 +28,40 @@ void Fibonacci::iteratif(int n)
         b = c;
     }
 }
+
+int Fibonacci::rekursif(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return rekursif(n - 1) + rekursif(n - 2);
+    }
+}
+
+int main()
+{
+    Fibonacci fib;
+
+    int n, a;
+
+    cout << "Masukkan Bilangan: ";
+    cin >> n;
+    cout << "Fibonacci (" << n << ")" << endl;
+    cout << "Iteratif: ";
+    fib.iteratif(n);
+    cout << endl;
+    cout << "Rekursif: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << fib.rekursif(i) << " ";
+    }
+    cout << endl;
+    return 0;
+}
